b5aee356a225702c1ac582e7349d85160737db4f
rikusrocks <rikusrocks@gmail.com>
2023-04-03 14:02:35 +1000

    main structure

---
+++ src/components/leftpanel/leftpanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {style} from "./leftpanel.module.css"
+
+const LeftPanel = () => {
+    return (
+        <>
+        <div className="left-panel">
+        LeftPanel
+        </div>
+        </>
+    )
+}
+
+export default LeftPanel;
---
+++ src/components/taskbox/taskbox.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {style} from "./taskbox.module.css"
+
+const TaskBox = () => {
+    return(
+        <> 
+        <div className="task-box">TaskBox</div></>
+    )
+}
+
+export default TaskBox;
