cbec3308e0e953e5910bfecfdd10552512df40ec
rikusrocks <rikusrocks@gmail.com>
2023-04-17 20:28:27 +1000

    add task model

---
+++ src/components/addtask/addtask.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddTask = () => {
+    return (
+        <>
+            <div>
+                add task
+            </div>
+        </>
+    )
+}
+
+export default AddTask;
